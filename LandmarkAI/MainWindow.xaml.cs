b8235b8d8927d936bcbe03e628fb16086bf63748
Sunil Kumar <sunilpalarya@gmail.com>
2025-06-06 12:02:38 +0530

    image prediction is working

--- LandmarkAI/MainWindow.xaml.cs
+++ LandmarkAI/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Win32;
+﻿using LandmarkAI.Models;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -18,11 +23,42 @@ namespace LandmarkAI
         {
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filter = "Image files (*.png;*.jpg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
             if (dialog.ShowDialog() == true)
             {
                 string fileName = dialog.FileName;
                 selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                _ = MakePredictionAsync(fileName);
             }
+        }
+
+        private async Task MakePredictionAsync(string fileName)
+        {
+            string predictionKey = "c72ba08f5b8211149b5a";
+            string endpoint = "https://centralindia.api.cognitive.microsoft.com/"; // e.g., https://<region>.api.cognitive.microsoft.com/
+            string projectId = "4da347d-460c-963a-c9dc34a691b4";
+            string publishedModelName = "Iteration1";
+
+            string url = $"{endpoint}/customvision/v3.0/Prediction/{projectId}/classify/iterations/{publishedModelName}/image";
+            string content_type = "application/octet-stream";
+
+            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+
+            byte[] imageData = File.ReadAllBytes(fileName);
+            using (var content = new ByteArrayContent(imageData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue(content_type);
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                string result = await response.Content.ReadAsStringAsync();
+
+                var predictions = JsonConvert.DeserializeObject<CustomVision>(result)?.Predictions;
+                listViewPredictions.ItemsSource = predictions;
+            }
+
         }
     }
 }
